b5ade48d66d2517a9b66b6c361265ea01bb43d95
Matias <matiasmammarella@gmail.com>
2023-02-09 23:44:23 -0300

    Agregue funciones en JS

--- functions/functions.js
+++ functions/functions.js
@@ -1,8 +1,13 @@
-var coordinates, url;
+var url;
 
-document.getElementById("map").onclick, function(){
-    coordinates = $('#coordinates').val();
-    url         = "https://www.google.com.sa/maps/search/"+ coordinates +",12.21z?hl=en";
-  
-    window.open(url, '_blank');
-  };
+function GoogleMaps() {
+  url = "https://www.google.com.ar/maps/place/San+Mart%C3%ADn,+Provincia+de+Buenos+Aires/@-34.5759934,-58.5462005,15z/data=!3m1!4b1!4m6!3m5!1s0x95bcb77160243783:0x5e21a2d3c0a65957!8m2!3d-34.5757521!4d-58.5370965!16s%2Fm%2F03c_642";
+  var win = window.open(url, '_blank');
+  win.focus();
+}
+
+function GoogleMail() {
+  url = "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=rodrigogomez@gmail.com";
+  var win = window.open(url, '_blank');
+  win.focus();
+}
